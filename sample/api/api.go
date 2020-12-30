3e895d0a99207358fa8005ec9d76b5d1d64d739d
zhaomingqiang <zhaomingqiang@bytedance.com>
2020-12-30 20:26:27 +0800

    add send card message sample

--- sample/api/api.go
+++ sample/api/api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/larksuite/oapi-sdk-go/api"
 	"github.com/larksuite/oapi-sdk-go/api/core/request"
@@ -18,8 +19,41 @@ var conf = test.GetInternalConf("online")
 
 func main() {
 	//testSendMessage()
-	testUploadFile()
+	testSendCardMessage()
+	//testUploadFile()
 	//testDownloadFile()
+}
+
+// send card message
+func testSendCardMessage() {
+	coreCtx := core.WrapContext(context.Background())
+	cardContent := "{\"config\":{\"wide_screen_mode\":true},\"i18n_elements\":{\"zh_cn\":[{\"tag\":\"div\",\"text\":{\"tag\":\"lark_md\",\"content\":\"[飞书](https://www.feishu.cn)整合即时沟通、日历、音视频会议、云文档、云盘、工作台等功能于一体，成就组织和个人，更高效、更愉悦。\"}},{\"tag\":\"action\",\"actions\":[{\"tag\":\"button\",\"text\":{\"tag\":\"plain_text\",\"content\":\"主按钮\"},\"type\":\"primary\",\"value\":{\"key\":\"primary\"}},{\"tag\":\"button\",\"text\":{\"tag\":\"plain_text\",\"content\":\"次按钮\"},\"type\":\"default\",\"value\":{\"key\":\"default\"}}]}]}}"
+	card := map[string]interface{}{}
+	err := json.Unmarshal([]byte(cardContent), &card)
+	if err != nil {
+		panic(err)
+	}
+	body := map[string]interface{}{
+		"open_id":  "[open_id]",
+		"msg_type": "interactive",
+		"card":     card,
+	}
+	ret := make(map[string]interface{})
+	req := request.NewRequestWithNative("message/v4/send", "POST",
+		request.AccessTokenTypeTenant, body, &ret,
+		//应用市场应用 request.SetTenantKey("TenantKey"),
+	)
+	err = api.Send(coreCtx, conf, req)
+	fmt.Println(coreCtx.GetRequestID())
+	fmt.Println(coreCtx.GetHTTPStatusCode())
+	if err != nil {
+		fmt.Println(tools.Prettify(err))
+		e := err.(*response.Error)
+		fmt.Println(e.Code)
+		fmt.Println(e.Msg)
+		return
+	}
+	fmt.Println(tools.Prettify(ret))
 }
 
 // send message
