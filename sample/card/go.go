6fb454bacfde35520e13fb5ffe5c654bbfc54926
zhaomingqiang <zhaomingqiang@bytedance.com>
2021-08-05 17:55:20 +0800

    modify sample

--- sample/card/go.go
+++ sample/card/go.go
@@ -25,11 +25,12 @@ func main() {
 	header := make(map[string][]string)
 	// from http request header
 	// and "X-Lark-Request-Timestamp"/"X-Lark-Request-Nonce"/"X-Lark-Signature" validate request, Required!
-	header["X-Request-Id"] = []string{"63278309j-yuewuyeu-7828389"}
-	header["X-Lark-Request-Timestamp"] = []string{"Monday, 09-Nov-20 23:33:53 CST"}
-	header["X-Lark-Request-Nonce"] = []string{"0404f57f-102e-4c91-bb32-a501ad0b7600"}
-	header["X-Lark-Signature"] = []string{"26cb59f4f5a91c4147d0xxxxxxxxxc4a36fb2c"}
-	header["X-Refresh-Token"] = []string{"acc4d5f2-4bc6-4394-a9d4-45e168fcde97"}
+
+	// header["X-Request-Id"] = []string{"63278309j-yuewuyeu-7828389"}
+	// header["X-Lark-Request-Timestamp"] = []string{"Monday, 09-Nov-20 23:33:53 CST"}
+	// header["X-Lark-Request-Nonce"] = []string{"0404f57f-102e-4c91-bb32-a501ad0b7600"}
+	// header["X-Lark-Signature"] = []string{"26cb59f4f5a91c4147d0xxxxxxxxxc4a36fb2c"}
+	// header["X-Refresh-Token"] = []string{"acc4d5f2-4bc6-4394-a9d4-45e168fcde97"}
 
 	req := &core.OapiRequest{
 		Ctx:    context.Background(),
