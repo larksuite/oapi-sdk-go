3f16b0d328912adadc09d435ea486c55781709d6
Ryanbeta <theheadofalion@126.com>
2025-05-28 10:09:12 +0800

    Update model.go
    card model说明这里更新下？

--- event/dispatcher/callback/model.go
+++ event/dispatcher/callback/model.go
@@ -60,7 +60,7 @@ type Toast struct {
 type Card struct {
 	// template/raw
 	Type string `json:"type,omitempty"`
-	// type为raw时：data为larkcard.MessageCard; type为raw时，data为TemplateCard
+	// type为raw时：data为larkcard.MessageCard; type为template时，data为TemplateCard
 	Data interface{} `json:"data,omitempty"`
 }
 
